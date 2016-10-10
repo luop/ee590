d9f84f0d98167a9ed24655f273de9220dd809bed
Luo Pan <lpan@linux-lab-001.ee.washington.edu>
2016-10-10 00:55:25 +0000

    Completed problem 4

--- hw2/prob4/day.c
+++ hw2/prob4/day.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include "day.h"
 
-char **daytab;
-daytab = (char **)malloc(sizeof(char *) * 2);
-daytab[0] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-daytab[1] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//char **daytab;
+//daytab = (char **)malloc(sizeof(char *) * 2);
+//daytab[0] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//daytab[1] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+static int daytab[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int day_of_year(int year, int month, int day){
   int i, leap;
 
-  leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+  leap = ( ( year%4 == 0 ) && ( year%100 != 0 ) ) || ( year%400 == 0 );
 
   for (i = 1; i < month; i ++){
-    day += daytab[leap][i];
+    //day += daytab[leap][i];
+    day += (*(daytab + leap * 13 + i));
   }
 
-  return day
+  return day;
 }
 
 void month_day(int year, int yearday, int *pmonth, int *pday){
 
   int i, leap;
 
-  leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+  leap = ( ( year%4 == 0 ) && ( year%100 != 0 ) ) || ( year%400 == 0 );
 
-  for (i = 1; yearday > ; i ++){
-    yearday -= daytab[leap][i];
+  for (i = 1; yearday > (*(daytab + leap * 13 + i)); i ++){
+    //yearday -= daytab[leap][i];
+    yearday -= (*(daytab + leap * 13 + i));
   }
 
   *pmonth = i;
--- hw2/prob4/main.c
+++ hw2/prob4/main.c
@@ -5,19 +5,21 @@
 
 int main( int argc, char * argv[] ) {
 
-  if ( argc == 3 || ) {
+  if ( argc == 3 ) {
     int year = atoi(argv[1]);
     int yearday = atoi(argv[2]);
 
-    int *pmonth;
-    int *pday;
+    int pmonth = 0;
+    int pday = 0;
 
-    month_day(year, yearday, *pmonth, *pday);
+    month_day(year, yearday, &pmonth, &pday);
+    printf("%d %d\n", pmonth, pday);
+
   }else if ( argc == 4 ){
     int year = atoi(argv[1]);
     int month = atoi(argv[2]);
-    int month = atoi(argv[3]);
-    printf("%d",day_of_year(year, month, day));
+    int day = atoi(argv[3]);
+    printf("%d\n",day_of_year(year, month, day));
   }else {
     printf("Usage: %s year <the day of year> - Month/Day\n", argv[0]);
     printf("Usage: %s year month day - the day of year\n", argv[0]);
