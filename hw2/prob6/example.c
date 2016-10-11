cc8151aa3655c5790f200707b14310b8ea6cbfe3
Luo Pan <lpan@linux-lab-001.ee.washington.edu>
2016-10-11 05:05:21 +0000

    Implemented the inverse of a matrix

---
+++ hw2/prob6/example.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "matrix.h"
+
+int main ( int argc, char * argv[] ) {
+
+  Matrix * A = matrix_new ( 2, 2 ),
+         * B = matrix_random ( 4, 4 ),
+         * C, * D;
+
+  matrix_set ( A, 0, 0, 1 ); matrix_set ( A, 0, 1, 2 );
+  matrix_set ( A, 1, 0, 3 ); matrix_set ( A, 1, 1, 4 );
+
+  C = matrix_add ( A, A );
+  matrix_print ( C ); NL;
+  matrix_destroy ( C );
+
+  matrix_print ( B ); NL;
+  C = matrix_minor ( B, 1 );
+  matrix_print ( C ); NL;
+  matrix_destroy ( C );
+
+  printf ( "det = %lf\n", matrix_det ( B ) );
+
+  C = matrix_identity ( 3 );
+  matrix_print(C); NL;
+  matrix_destroy(C);
+
+  C = matrix_scale ( A, 0.2 );
+  matrix_print(C); NL;
+  matrix_destroy(C);
+
+  C = matrix_transpose ( A );
+  matrix_print(C); NL;
+  matrix_destroy(C);
+
+  C = matrix_mult ( A, A );
+  matrix_print(C); NL;
+
+  D = matrix_power ( A, 2 );
+  matrix_print(D); NL;
+
+  printf ("Are they equal? %d\n", matrix_equal ( C, D ));
+
+  printf ("Are they equal? %d\n", matrix_equal ( A, B ));
+
+  printf ("Are they equal? %d\n", matrix_equal ( A, C ));
+
+  matrix_destroy(C);
+  matrix_destroy(D);
+
+  C = matrix_from_file ( "data/m1.mat" );
+  matrix_print(C); NL;
+  printf ( "det = %lf\n", matrix_det ( C ) );
+
+  D = matrix_cofactor (C);
+  matrix_print ( D ); NL;
+  matrix_destroy(D);
+
+  D = matrix_inverse ( C );
+  matrix_print ( D ); NL;
+  matrix_print ( matrix_mult ( C, D ) ); NL;
+  matrix_destroy(C);
+  matrix_destroy(D);
+
+  return 0;
+
+}
---
+++ hw2/prob6/matrix.h
@@ -0,0 +1,54 @@
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+typedef struct {
+  double * value;
+  int rows;
+  int columns;
+} Matrix;
+
+/* allocation and setup */
+Matrix * matrix_new ( int rows, int columns );
+void matrix_destroy ( Matrix * M );
+Matrix * matrix_random ( int rows, int columns );
+Matrix * matrix_from_file ( const char filename[] );
+
+/* Getters and setters */
+double matrix_get ( Matrix * M, int row, int column );
+void matrix_set ( Matrix * M, int row, int column, double value );
+
+/* print */
+void matrix_print ( Matrix * M );
+
+/* operations */
+Matrix * matrix_add ( Matrix * A, Matrix * B );
+Matrix * matrix_minor ( Matrix * M, int n );
+double matrix_det ( Matrix * M );
+
+// problem 5
+int matrix_equal ( Matrix * A, Matrix * B );      /* return 1 if A == B (element wise) and 0 otherwise */
+Matrix * matrix_identity ( int n );               /* return the nxn identity matrix */
+Matrix * matrix_scale ( Matrix * M, double s );   /* return s * M, multiplying each element of M by s */
+Matrix * matrix_transpose ( Matrix * M );         /* return the transpose of M */
+Matrix * matrix_mult ( Matrix * A, Matrix * B );  /* return A*B using matrix multiplication */
+Matrix * matrix_power ( Matrix * M, int n );      /* return M^n = M * M * ... * M (n times ) with n >=0*/
+
+// problem 6
+double matrix_cofactor_element ( Matrix * M, int m, int n );    /* returns each element of cofactor */
+Matrix * matrix_cofactor (Matrix * M);            /* returns the cofactor of M */
+
+Matrix * matrix_inverse ( Matrix * M );          /* returns the inverse of M */
+
+
+/* utilities */
+#define ASSERT(_cond_) {                                                          \
+  if ( ! ( _cond_ ) ) {                                                           \
+    fprintf (stderr, "Internal error: assertion '%s' failed at %s, line %d\n",    \
+                   #_cond_ , __FILE__, __LINE__);                                 \
+    exit ( -1 );                                                                  \
+  }                                                                               \
+}
+
+#define NL printf ( "\n" );
+
+#endif
