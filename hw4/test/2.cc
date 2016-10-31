5bdc056066e1ca87ed33205810fb3dac385c4122
Luo Pan <lpan@linux-lab-001.ee.washington.edu>
2016-10-31 00:29:47 +0000

    Tested item 2

--- hw4/test/2.cc
+++ hw4/test/2.cc
@@ -1,20 +1,44 @@
 #include <iostream>
+#include <stdexcept>
 #include "json.hh"
 
 int main ( int argc, char * argv[] ) {
+
+  Number number(1.0);
+  Array array;
+
+  array.set(0,number);
+  number.set(2.0);
+  array.set(1,number);
+
+  try {
+     array.get(-1);
+  } catch ( json_exception &e ) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  try {
+     array.get(3);
+  } catch ( json_exception &e ) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  try {
+     array.get(10);;
+  } catch ( json_exception &e ) {
+    std::cerr << e.what() << std::endl;
+  }
 
   Hash h;
   Number n(1);
 
   h.set("first", n);
-  h.set("second", h);
 
-  std::cout << h.stringify() << std::endl;
-  std::cout << h.get("first")->stringify() << std::endl;
-  std::cout << h.get("second")->stringify() << std::endl;
-
-  // next line will fail until the copy constructor for Hash is implemented
-  std::cout << h.get("second")->get("first")->stringify() << std::endl;
+  try {
+     h.get("second");
+  } catch ( json_exception &e ) {
+    std::cerr << e.what() << std::endl;
+  }
 
   return 0;
 
