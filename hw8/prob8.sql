a000e9eeab0a9e2025ac384c88e8704f36660051
Luo Pan <lpan@linux-lab-001.ee.washington.edu>
2016-12-07 08:15:07 +0000

    Updated prob8

--- hw8/prob8.sql
+++ hw8/prob8.sql
@@ -1,6 +1,6 @@
 select ar.name, sum(il.UnitPrice * il.Quantity) as TotalSpent
-from Invoice i, InvoiceLine il, Track t, Album a, Artist ar
-where i.InvoiceId = il.InvoiceId and il.TrackId = t.TrackId and t.AlbumId = a.AlbumId and a.ArtistId = ar.ArtistId
+from InvoiceLine il, Track t, Album a, Artist ar
+where il.TrackId = t.TrackId and t.AlbumId = a.AlbumId and a.ArtistId = ar.ArtistId
 group by ar.ArtistId
 order by TotalSpent desc
 limit 10;
