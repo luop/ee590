3286a7b90fedc28c2995f90a26468e221d94860f
Luo Pan <lpan@linux-lab-001.ee.washington.edu>
2016-12-07 07:26:29 +0000

    Updated prob3 and prob8

--- hw8/prob3.sql
+++ hw8/prob3.sql
@@ -1,6 +1,6 @@
 select c.FirstName, c.LastName, sum(i.total) as TotalSpent
-from Invoice i, InvoiceLine il, Customer c
-where i.InvoiceId = il.InvoiceId and i.CustomerId = c.CustomerId
+from Invoice i
+inner join Customer c on i.CustomerId = c.CustomerId
 group by i.CustomerId
 order by TotalSpent desc
 limit 10;
--- hw8/prob8.sql
+++ hw8/prob8.sql
@@ -1,4 +1,4 @@
-select ar.name, sum(i.total) as TotalSpent
+select ar.name, sum(il.UnitPrice * il.Quantity) as TotalSpent
 from Invoice i, InvoiceLine il, Track t, Album a, Artist ar
 where i.InvoiceId = il.InvoiceId and il.TrackId = t.TrackId and t.AlbumId = a.AlbumId and a.ArtistId = ar.ArtistId
 group by ar.ArtistId
